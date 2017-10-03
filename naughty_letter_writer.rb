d472e350da149a8c72b81a25f7d45137203c885a
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-03 18:34:56 -0400

    compelted the letters had trouble with the file names

--- naughty_letter_writer.rb
+++ naughty_letter_writer.rb
@@ -1,7 +1,7 @@
 require 'erb'
 
 kids_data   = File.read('data/kids-data.txt')
-nice_letter = File.read('templates/nice_letter_template.txt.erb')
+naughty_letter = File.read('templates/naughty_sample_letter.txt.erb')
 
 kids_data.each_line do |kid|
 
@@ -11,11 +11,24 @@ kids_data.each_line do |kid|
   gender   = kid_data_array[1]
   behavior = kid_data_array[2]
   toys     = kid_data_array[3..8]
+  infraction = kid.split('|')[1]
+  in_stock = toys.delete("Kaleidoscope")
+  random_toy = toys.sample
+  what_you_dont_get = toys.reject { |toy| toy == random_toy  }
+
+  # infraction = kid_data_array[15..-1]
+# puts kid_data_array
+
+# puts infraction
+# puts '-'*44
+# puts random_toy
+# puts '-'*33
+# puts what_you_dont_get.join(", ")
 
   next unless behavior == 'naughty'
 
   filename    = 'letters/naughty/' + name + '.txt'
-  letter_text = ERB.new(naughty_letter, nil, '|').result(binding)
+  letter_text = ERB.new(naughty_letter, nil, '-').result(binding)
 
   puts "Writing #{filename}."
   File.write(filename, letter_text)
